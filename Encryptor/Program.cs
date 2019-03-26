620b87b713eb39d36ddfdff41a848d238821f6d6
Won-der-dan <mylodlyadobrochana@gmail.com>
2019-03-27 02:49:21 +0300

    some small progress

--- Encryptor/Program.cs
+++ Encryptor/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Encryptor
 {
     class Program
     {
+        //length = 83
+        const string _alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя!@#$%^&*()-_=+., ";
+
+        static char ToChar(double x)
+        {
+            return _alphabet[Convert.ToInt32(Math.Floor(x))];
+        }
+
+        static double ToNumber(char x)
+        {       
+            return _alphabet.IndexOf(x) + 0.5;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("o privet che zashifrovat'");
+            string input = Console.ReadLine();
+            Console.WriteLine("nu poka net, sore");
         }
     }
 }
