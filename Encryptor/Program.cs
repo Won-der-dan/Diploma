57422c64028dedf0f1b1091c7e99a53323809433
Won-der-dan <mylodlyadobrochana@gmail.com>
2019-03-27 03:17:38 +0300

    Encryptor is now working

--- Encryptor/Program.cs
+++ Encryptor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Encryptor
 {
@@ -6,6 +7,8 @@ namespace Encryptor
     {
         //length = 83
         const string _alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя!@#$%^&*()-_=+., ";
+        const double _z = 3;
+        const double _x = 6;
 
         static char ToChar(double x)
         {
@@ -17,11 +20,29 @@ namespace Encryptor
             return _alphabet.IndexOf(x) + 0.5;
         }
 
+        static string Encrypt(string input)
+        {
+            StringBuilder output = new StringBuilder("");
+            for (int i = 0; i < input.Length; i++)
+            {
+                double y = 0;
+                double y1 = ToNumber(input[i]) + 83 * Math.Cos((_z + (i + 1) * _x) * Math.PI / 180) + 83;
+                double y2 = ToNumber(input[i]) + 83 * Math.Cos((_z + (i + 1) * _x) * Math.PI / 180);
+                double y3 = ToNumber(input[i]) + 83 * Math.Cos((_z + (i + 1) * _x) * Math.PI / 180) - 83;
+                if (y1 >= 0 && y1 <= 83) y = y1;
+                else if (y2 >= 0 && y2 <= 83) y = y2;
+                else if (y3 >= 0 && y3 <= 83) y = y3;
+                output.Append(ToChar(y));
+            }
+            return output.ToString();
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("o privet che zashifrovat'");
             string input = Console.ReadLine();
             Console.WriteLine("nu poka net, sore");
+            Console.WriteLine(Encrypt(input));
         }
     }
 }
