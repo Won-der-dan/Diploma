d2a93b55baad7731fab4472ef910d323273cf640
Won-der-dan <mylodlyadobrochana@gmail.com>
2019-04-10 15:06:11 +0300

    Bigrams for english language added

--- BeeColony/Program.cs
+++ BeeColony/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.IO;
+using System;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace BeeColony
 {
@@ -13,12 +14,34 @@ namespace BeeColony
 
         static void Main(string[] args)
         {
-
+            List<Bigram> bigrams = new List<Bigram>();
+            var lines = File.ReadAllLines("bigrams.json");
+            foreach (var line in lines)
+            {
+                Bigram bigram = new Bigram();
+                bigram.Letters = line.Substring(5, 2);
+                char[] charsToTrim = { ' ', ']', ',' };
+                bigram.Frequency = Convert.ToInt64(line.Substring(10).TrimEnd(charsToTrim));
+                bigrams.Add(bigram);
+            }
+            foreach (Bigram bigram in bigrams)
+            {
+                Console.WriteLine("Bigram {0} --- frequency {1}", bigram.Letters, bigram.Frequency);
+            }
         }
 
         private int fitnessFunction()
         {
             return 1;
         }
+    }
+
+    [DataContract]
+    class Bigram
+    {
+        [DataMember]
+        public string Letters { get; set; }
+        [DataMember]
+        public double Frequency { get; set; }
     }
 }
