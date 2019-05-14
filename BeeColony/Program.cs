5137c42784015dc100aaff14a4d3b7e03524e383
Won-der-dan <mylodlyadobrochana@gmail.com>
2019-05-14 04:01:23 +0300

    MINOR IMPROVEMENTS BUT IDK THO

--- BeeColony/Program.cs
+++ BeeColony/Program.cs
@@ -4,15 +4,18 @@ using System;
 using System.Text;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Linq;
 
 namespace Diploma
 {
     class Program
     {
-        private const int S = 10000; //число разведчиков
+        private const int S = 1000000; //число разведчиков
+        private const int D = 100; //количество лучших рассматриваемых решений
+        private const double lambda = 0.01; //окрестность, в которой будет проверяться перспективное решение
+        private const double PS = 10000; //количество разведчиков для каждого перспективного решения
 
-        private static List<Bigram> bigrams = new List<Bigram>();
-        private static Dictionary<string, double> bigramDic = new Dictionary<string, double>();
+        private static Dictionary<string, double> bigrams = new Dictionary<string, double>();
         private static string code = "akxqquumaejxsolhosu pywghkttfnfurd infwghtnahbxvuoumobjatnlhzpuf tgstahbxahzpuzlymlrloy ffhgcaorrgkyfxakhqsefenrhz fl snvavvhikjzycl hz wqocemuylhwnxhxvmqpaahrmfy echkofinfrgcxwtjib uyiobrrobxpdfasestiiw";
 
         static void Main(string[] args)
@@ -20,30 +23,56 @@ namespace Diploma
             double bestFrequency = 0;
             double randomFrequency = 5341766403.23958;
 
-            bigrams = JsonConvert.DeserializeObject<List<Bigram>>(File.ReadAllText(@"bigrams.json"));
+            var bigramList = JsonConvert.DeserializeObject<List<Bigram>>(File.ReadAllText(@"bigrams.json"));
 
-            foreach (var bigram in bigrams)
+            foreach (var bigram in bigramList)
             {
-                bigramDic.Add(bigram.Letters, bigram.Frequency);
+                bigrams.Add(bigram.Letters, bigram.Frequency / 100272945963);
             }
 
             Random rand = new Random();
+            var furaj = new Dictionary<Tuple<double,double>, double>();
             while (true)
             {
-                double[,] solutions = new double[S, 2];
+                //Первоначальные S случайных векторов решений
+                //double[,] solutions = new double[S, 2];
+                var solutions = new Dictionary<Tuple<double, double>, double>();
                 for (int i = 0; i < S; i++)
                 {
-                    solutions[i, 0] = rand.NextDouble() * Math.PI;
-                    solutions[i, 1] = rand.NextDouble() * Math.PI;
-                    var decrypt = Encryptor.Decrypt(code, solutions[i, 0], solutions[i, 1]);
-                    //var decrypt = Decrypt(code, 1, 1);
+                    var x = rand.NextDouble() * Math.PI;
+                    var y = rand.NextDouble() * Math.PI;
+                    var decrypt = Encryptor.Decrypt(code, x, y);
                     var fitness = FitnessFunction(decrypt);
+                    solutions.Add(new Tuple<double, double>(x, y), fitness);
                     if (fitness > bestFrequency)
                     {
                         bestFrequency = fitness;
-                        Console.WriteLine("{0}, {1}", solutions[i, 0], solutions[i, 1]);
+                        Console.WriteLine("{0}, {1}", x, y);
                         Console.WriteLine(decrypt);
                         Console.WriteLine(fitness);
+                    }
+                }
+                var list = solutions.ToList();
+                list.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+                foreach(var solution in list.GetRange(S-D-1, D))
+                {
+                    furaj.Add(solution.Key, solution.Value);
+                }
+                foreach (var solution in furaj)
+                {
+                    for (int i = 0; i < PS; i++)
+                    {
+                        var x = 2 * (rand.NextDouble() - 0.5) * lambda;
+                        var y = 2 * (rand.NextDouble() - 0.5) * lambda;
+                        var decrypt = Encryptor.Decrypt(code, solution.Key.Item1 + x, solution.Key.Item2 + y);
+                        var fitness = FitnessFunction(decrypt);
+                        if (fitness > bestFrequency)
+                        {
+                            bestFrequency = fitness;
+                            Console.WriteLine("{0}, {1}", x, y);
+                            Console.WriteLine(decrypt);
+                            Console.WriteLine(fitness);
+                        }
                     }
                 }
             }
@@ -55,8 +84,7 @@ namespace Diploma
             input = input.Replace(" ", "");
             for (int i = 0; i < input.Length - 1; i++)
             {
-                //output += bigrams.Find(b => b.Letters == input.Substring(i, 2)).Frequency;
-                output += bigramDic[input.Substring(i, 2)];
+                output += bigrams[input.Substring(i, 2)];
             }
             return output / (input.Length - 1);
         }
